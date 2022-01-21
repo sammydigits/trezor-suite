66c5e2b1e3a3e7aff7ba7e5d24f5ce4b022fbb3a
pingpwned <pingpwned@nblx.me>
2022-01-21 10:30:04 +0100

    fix(access settings): tooltip for disabled Switch

--- packages/suite/src/views/settings/general/Labeling.tsx
+++ packages/suite/src/views/settings/general/Labeling.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch } from '@trezor/components';
+import { Switch, Tooltip } from '@trezor/components';
 import { Translation } from '@suite-components';
 import { ActionColumn, SectionItem, TextColumn } from '@suite-components/Settings';
 import { useSelector, useActions, useDevice } from '@suite-hooks';
@@ -34,20 +34,34 @@ export const Labeling = () => {
                 description={<Translation id="TR_LABELING_FEATURE_ALLOWS" />}
             />
             <ActionColumn>
-                <Switch
-                    // hmm maybe it should never be disabled, as it is not device related option (although it triggers device flow?)
-                    // but on the other hand there still may be case when it remembered device is disconnected and its metadata.status
-                    // is cancelled or disabled. In such case, initMetadata does not make any sense as it needs device connected.
-                    // You could say: "ok, whatever, but this switch is changing only application setting, why messing with device?"
-                    // Yes, you are right, but if it was done this way, you would enable metadata, then go to wallet, discovery
-                    // and maybe device authorization would be triggered and only after that you would get metadata flow started, wouldn't
-                    // that be confusing? I believe it is better to do it right away, but need for disabling this switch in specific
-                    // edge case is a drawback.
-                    isDisabled={!metadata.enabled && !device?.connected && isDeviceLocked}
-                    data-test="@settings/metadata-switch"
-                    checked={metadata.enabled}
-                    onChange={() => (metadata.enabled ? disableMetadata() : initMetadata(true))}
-                />
+                <Tooltip
+                    maxWidth={280}
+                    offset={10}
+                    placement="top"
+                    dashed={false}
+                    cursor="not-allowed"
+                    content={
+                        !metadata.enabled &&
+                        (!device || !device?.connected || !device?.state) && (
+                            <Translation id="TR_DISABLED_SWITCH_TOOLTIP" />
+                        )
+                    }
+                >
+                    <Switch
+                        // hmm maybe it should never be disabled, as it is not device related option (although it triggers device flow?)
+                        // but on the other hand there still may be case when it remembered device is disconnected and its metadata.status
+                        // is cancelled or disabled. In such case, initMetadata does not make any sense as it needs device connected.
+                        // You could say: "ok, whatever, but this switch is changing only application setting, why messing with device?"
+                        // Yes, you are right, but if it was done this way, you would enable metadata, then go to wallet, discovery
+                        // and maybe device authorization would be triggered and only after that you would get metadata flow started, wouldn't
+                        // that be confusing? I believe it is better to do it right away, but need for disabling this switch in specific
+                        // edge case is a drawback.
+                        isDisabled={!metadata.enabled && !device?.connected && isDeviceLocked}
+                        data-test="@settings/metadata-switch"
+                        checked={metadata.enabled}
+                        onChange={() => (metadata.enabled ? disableMetadata() : initMetadata(true))}
+                    />
+                </Tooltip>
             </ActionColumn>
         </SectionItem>
     );
